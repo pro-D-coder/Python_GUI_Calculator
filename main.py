a49cae28401cc6eae35fb6790fe161067a3b92a1
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-01-20 19:05:48 +0530

    added comment for code understanding

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import math as m #importing math module for doing fast mathematical operations
 def check_operand(string): 
     for i in string:
         if i in ["+","-","÷","x","%","^"]:
-            return True
+            return True #return true if given string contains operators
 #function that add given input in Entry using buttons
 def add_on_scr(input,scr_data):
     if input in ["%","-","+","%","x","÷"]:   #if input is an operation
@@ -24,36 +24,38 @@ def add_on_scr(input,scr_data):
     elif input == ".": #checkinf if input is "."
         data = scr_data.get() #getting data form entry
         if check_operand(data) != True: #checking that expression contains operators or not
-            if data.count(".",0, len(data)) < 1:
+            if data.count(".",0, len(data)) < 1: #checking if operand already have . or not if not then add
                 expression = scr_data.get()
                 expression += input
                 scr_data.set(expression)
             else:
                 pass
         else:
-            data = scr_data.get()
-            list_of_operators = list()
-            for i in range(0, len(data)):
-                if(data[i] in ["+","-","÷","x","%","^"]):
-                    list_of_operators.append(i)
-            last_data = data[list_of_operators[len(list_of_operators) -1 ] + 1:]
-            if last_data.count(".", 0, len(last_data)) < 1:
+            data = scr_data.get() #getting data from entry
+            list_of_operators = list() #list of operators
+            for i in range(0, len(data)):   #getting indices of operators present in expression
+                if(data[i] in ["+","-","÷","x","%","^"]):      #
+                    list_of_operators.append(i)                #End here
+            last_data = data[list_of_operators[len(list_of_operators) -1 ] + 1:] #getting last value from expression and
+            if last_data.count(".", 0, len(last_data)) < 1: #checking that the value contains dot(.) or not if not then add
                 expression = scr_data.get()
                 expression += input
                 scr_data.set(expression)
-            else:
+            else:   #if last value contain dot(.) then do nothing
                 pass
-    else:
+    else: #if input is anything else then add it in entry
         expression = scr_data.get()
         expression += input
         scr_data.set(expression)
+
+#Function that evaluate the given expression used in = operation
 def give_anwser(scr_data):
     try:
-        expression = (str(scr_data.get()).replace("÷", "/")).replace("x","*")
-        expression = expression.replace("^","**")
-        expression = eval(expression)
-        scr_data.set(expression)
-    except ZeroDivisionError as e:
+        expression = (str(scr_data.get()).replace("÷", "/")).replace("x","*") #getting the expression from entry with replacement of "÷" to "/" and "x" to "*"
+        expression = expression.replace("^","**") #and also replacing "^" sign to "**" for python interpreter 
+        expression = eval(expression) #evaluating the expression
+        scr_data.set(expression)  #setting the entry to solution
+    except ZeroDivisionError as e: #if number is divided by zero except Exception and setting enrty to infinite sign
         scr_data.set("∞")
     except NameError as e:
         print(e)
@@ -64,87 +66,99 @@ def give_anwser(scr_data):
         messagebox.showerror("Error","Please correct your expression!!")
     except Exception as e:
         print(e)
+
+#Funtion that used to clearing the entry
 def clear_scr(scr_data):
     scr_data.set("")
+
+#Function that remove singal value from right side of expression
 def Backspace(scr_data):
-    data = scr_data.get()
-    data = data[:len(data)-1]
-    scr_data.set(data)
+    data = scr_data.get() #getting data
+    data = data[:len(data)-1] #using slicing technic to remove only rightmost value or number or operator
+    scr_data.set(data) #setting the entry
+
+#Function that used to add - sign on operands
 def put_sign(scr_data):
-    data = scr_data.get()
-    list_of_operators = []
+    data = scr_data.get() #getting data from entry
+    list_of_operators = [] #list that contain indices of operators
+    #checking that entry contain operators or not 
     if check_operand(data) != True:
-        if len(data) >=1 and data not in ".":
-            scr_data.set("-" + scr_data.get())
-        else:
+        if len(data) >=1 and data not in ".": #if not then check length of the expression and also check that the last value is a dot(.) if dot then do not add sign on it.
+            scr_data.set("-" + scr_data.get()) #adding sign on given data 
+        else: #else do nothing
             return
-    else:
-        for i in range(0, len(data)):
+    else: #if entry contains operator
+        for i in range(0, len(data)): #then find index of all operators
             if(data[i] in ["+","-","÷","x","%","^"]):
                 list_of_operators.append(i)
-        if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1:
-            last_data_with_sign = "("+"-" + data[list_of_operators[len(list_of_operators) -1 ] + 1:] + ")"
-            scr_data.set(data[:list_of_operators[len(list_of_operators) -1 ] + 1]+last_data_with_sign)
+        if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1: #here checking that after an operand there is an value or not if not then do not add - sign 
+            last_data_with_sign = "("+"-" + data[list_of_operators[len(list_of_operators) -1 ] + 1:] + ")" #if yes then get last value of expression and add - sign on it.
+            scr_data.set(data[:list_of_operators[len(list_of_operators) -1 ] + 1]+last_data_with_sign) #here taking entry value with no sign and adding it with sign add value
+
+#Function that add ^ sign to expression and solve it if there is already an ^ sign  
 def x_power_a(scr_data):
-    data = scr_data.get()
-    index_of_operand = list()
-    for i in range(len(data)):
+    data = scr_data.get()   #getting expression
+    index_of_operand = list()   #list of operators
+    for i in range(len(data)):  #getting indices of operators present in expression
         if data[i] in ["+","-","÷","x","%","^"]:
             index_of_operand.append(i)
-    if check_operand(data) == True:
-        if data[len(data) -1] not in ["+","-","÷","x","%","^","."]:
-            if data[index_of_operand[len(index_of_operand) -1]] == "^":
-                        if len(index_of_operand) == 1:
+    if check_operand(data) == True:  #checking that expression contains operators or not
+        if data[len(data) -1] not in ["+","-","÷","x","%","^","."]: #checking if last value of expression contain operators if yes then do not add ^ sign
+            if data[index_of_operand[len(index_of_operand) -1]] == "^": #checking the right most operator is "^" if yes then solve already ^ sign
+                        if len(index_of_operand) == 1: #here if number of operands then directly solve the power expression and set it 
                             try:
                                 scr_data.set(str(eval(data.replace("^","**"))) + "^") 
                             except:
                                 pass
-                        else:    
+                        else: #if number operand is greater then 1 then do this    
                             try:
-                                if data[len(data) -1] in ["+","-","÷","x","%","^"]:
+                                if data[len(data) -1] in ["+","-","÷","x","%","^"]: #if rightmost part of expression is a operator then do not add ^ sign
                                     pass
-                                else:
+                                else: #Else solve first power equation and then add another "^" sign on it
                                     last_sol = eval(data[index_of_operand[len(index_of_operand) -2] + 1:].replace("^","**"))
                                     scr_data.set(data[:index_of_operand[len(index_of_operand) -2] + 1]+str(last_sol) + "^")
                             except:
-                                messagebox.showwarning("Error","Please Correct Your Expression!")
-                            
+                                messagebox.showwarning("Error","Please Correct Your Expression!")           
             else:
                 scr_data.set(data + "^")
-    else:
+    else: #else if there is no operand in expression then add "^" sign
         if len(data) >= 1 and data[len(data) -1] not in ["+","-","÷","x","%","^","."]:
             scr_data.set(data + "^")
+
+#Function that find squareroot
 def sqrt_ans(scr_data):
-    data = scr_data.get()
+    data = scr_data.get() #getting entry data
     try:
-        if check_operand(data) != True:
-            if len(data) >= 1:
+        if check_operand(data) != True: #checking that entry contain operators or not
+            if len(data) >= 1: #if there is no operator check length of entry if greater then one then solve the equation
                 scr_data.set(m.sqrt(eval(data)))
     except:
         pass
-    else:
+    else: #if there is operators then do this
         try:
-            list_of_operators = list()
-            for i in range(0, len(data)):
+            list_of_operators = list() #list of operators
+            for i in range(0, len(data)):   #getting indices of operators present in expression
                 if(data[i] in ["+","-","÷","x","%"]):
                     list_of_operators.append(i)
-            if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1:
-                sqr_with_exp = data[:list_of_operators[len(list_of_operators) -1 ] + 1] + str(m.sqrt(eval(data[list_of_operators[len(list_of_operators) -1 ] + 1:])))
-                scr_data.set(sqr_with_exp)
+            if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1: #checking the length of rightmost value after an operator if greater then one find squareroot of rightmost value
+                sqr_with_exp = data[:list_of_operators[len(list_of_operators) -1 ] + 1] + str(m.sqrt(eval(data[list_of_operators[len(list_of_operators) -1 ] + 1:])))  #here adding squareroot of to the rightmost value with left expression
+                scr_data.set(sqr_with_exp) #setting up the entry
         except:
             pass
+
+#Function that remove the whole rightmost value
 def CE_ope(scr_data):
-    data = scr_data.get()
-    if check_operand(data) != True:
-        if len(data) >= 1:
+    data = scr_data.get() #getting entry data
+    if check_operand(data) != True: #checking that entry contain operators or not
+        if len(data) >= 1: #if not then set entry to empty
             scr_data.set("")
-    else:
-        index_of_operand = list()
-        for i in range(0, len(data)):
+    else: #Else 
+        index_of_operand = list() #find index of all operators
+        for i in range(0, len(data)): 
             if data[i] in ["+","-","÷","x","%","^"]:
                 index_of_operand.append(i)
-        new_value = data[:index_of_operand[len(index_of_operand)-1]+1]
-        scr_data.set(new_value)
+        new_value = data[:index_of_operand[len(index_of_operand)-1]+1] #here extracting the expression without the rightmost value
+        scr_data.set(new_value) #setting the new value
 #Main Function for whole programe.
 def main():
     main_win = tk.Tk() #creating instance of TK class.
