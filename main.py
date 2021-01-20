1c769f71afc84860da47d61ad837846fcba5827d
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-01-20 17:13:41 +0530

    added x to power a operation and removed some bug

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
 import math as m
-
 def check_operand(string): 
     for i in string:
-        if i in ["+","-","÷","x","%"]:
+        if i in ["+","-","÷","x","%","^"]:
             return True
 def add_on_scr(input,scr_data):
     if input in ["%","-","+","%","x","÷"]:
         data = scr_data.get()
         try:
-            if data[len(data) - 1] in ["%","-","+","%","x","÷"]:
+            if data[len(data) - 1] in ["%","-","+","%","x","÷","^","."]:
                 pass
             else:
                 expression = scr_data.get()
@@ -31,7 +30,7 @@ def add_on_scr(input,scr_data):
             data = scr_data.get()
             list_of_operators = list()
             for i in range(0, len(data)):
-                if(data[i] in ["+","-","÷","x","%"]):
+                if(data[i] in ["+","-","÷","x","%","^"]):
                     list_of_operators.append(i)
             last_data = data[list_of_operators[len(list_of_operators) -1 ] + 1:]
             if last_data.count(".", 0, len(last_data)) < 1:
@@ -47,6 +46,7 @@ def add_on_scr(input,scr_data):
 def give_anwser(scr_data):
     try:
         expression = (str(scr_data.get()).replace("÷", "/")).replace("x","*")
+        expression = expression.replace("^","**")
         expression = eval(expression)
         scr_data.set(expression)
     except ZeroDivisionError as e:
@@ -70,36 +70,46 @@ def put_sign(scr_data):
     data = scr_data.get()
     list_of_operators = []
     if check_operand(data) != True:
-        if len(data) >=1:
+        if len(data) >=1 and data not in ".":
             scr_data.set("-" + scr_data.get())
         else:
             return
     else:
         for i in range(0, len(data)):
-            if(data[i] in ["+","-","÷","x","%"]):
+            if(data[i] in ["+","-","÷","x","%","^"]):
                 list_of_operators.append(i)
         if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1:
             last_data_with_sign = "("+"-" + data[list_of_operators[len(list_of_operators) -1 ] + 1:] + ")"
             scr_data.set(data[:list_of_operators[len(list_of_operators) -1 ] + 1]+last_data_with_sign)
-def sqr_ans(scr_data):
+def x_power_a(scr_data):
     data = scr_data.get()
-    try:
-        if check_operand(data) != True:
-            if len(data) >= 1:
-                scr_data.set(eval(data)**2)
-    except:
-        pass
+    index_of_operand = list()
+    for i in range(len(data)):
+        if data[i] in ["+","-","÷","x","%","^"]:
+            index_of_operand.append(i)
+    if check_operand(data) == True:
+        if data[len(data) -1] not in ["+","-","÷","x","%","^","."]:
+            if data[index_of_operand[len(index_of_operand) -1]] == "^":
+                        if len(index_of_operand) == 1:
+                            try:
+                                scr_data.set(str(eval(data.replace("^","**"))) + "^") 
+                            except:
+                                pass
+                        else:    
+                            try:
+                                if data[len(data) -1] in ["+","-","÷","x","%","^"]:
+                                    pass
+                                else:
+                                    last_sol = eval(data[index_of_operand[len(index_of_operand) -2] + 1:].replace("^","**"))
+                                    scr_data.set(data[:index_of_operand[len(index_of_operand) -2] + 1]+str(last_sol) + "^")
+                            except:
+                                messagebox.showwarning("Error","Please Correct Your Expression!")
+                            
+            else:
+                scr_data.set(data + "^")
     else:
-        try:
-            list_of_operators = list()
-            for i in range(0, len(data)):
-                if(data[i] in ["+","-","÷","x","%"]):
-                    list_of_operators.append(i)
-            if len(data[list_of_operators[len(list_of_operators) -1 ] + 1:]) >= 1:
-                sqr_with_exp = data[:list_of_operators[len(list_of_operators) -1 ] + 1] + str(eval(data[list_of_operators[len(list_of_operators) -1 ] + 1:])**2)
-                scr_data.set(sqr_with_exp)
-        except:
-            pass
+        if len(data) >= 1 and data[len(data) -1] not in ["+","-","÷","x","%","^","."]:
+            scr_data.set(data + "^")
 def sqrt_ans(scr_data):
     data = scr_data.get()
     try:
@@ -119,6 +129,18 @@ def sqrt_ans(scr_data):
                 scr_data.set(sqr_with_exp)
         except:
             pass
+def CE_ope(scr_data):
+    data = scr_data.get()
+    if check_operand(data) != True:
+        if len(data) >= 1:
+            scr_data.set("")
+    else:
+        index_of_operand = list()
+        for i in range(0, len(data)):
+            if data[i] in ["+","-","÷","x","%","^"]:
+                index_of_operand.append(i)
+        new_value = data[:index_of_operand[len(index_of_operand)-1]+1]
+        scr_data.set(new_value)
 #Main Function for whole programe.
 def main():
     main_win = tk.Tk()
@@ -345,7 +367,7 @@ def main():
     font = ("Consolas",14,"bold"),
     padx = 26,
     pady = 24,
-    command = lambda:give_anwser(scr_data)
+    command = lambda:give_anwser(scr_data),
     )
     #Button for +/- operation
     button_for_sign = tk.Button(main_win,
@@ -401,7 +423,7 @@ def main():
     )
     #Button for square operation
     button_for_sqr = tk.Button(main_win,
-    text = "x²",
+    text = "xª",
     bg = "#233b44",
     fg = "#c9c5c2",
     relief = tk.FLAT,
@@ -410,7 +432,7 @@ def main():
     font = ("Consolas",14,"bold"),
     padx = 22,
     pady = 14,
-    command = lambda:sqr_ans(scr_data),
+    command = lambda:x_power_a(scr_data),
     )
     #Button for square root operation
     button_for_sqrt = tk.Button(main_win,
@@ -424,6 +446,45 @@ def main():
     padx = 26,
     pady = 24,
     command = lambda:sqrt_ans(scr_data),
+    )
+    #Button for ( 
+    button_for_left_brac = tk.Button(main_win,
+    text = "(",
+    bg = "#233b44",
+    fg = "#c9c5c2",
+    relief = tk.FLAT,
+    bd = 0,
+    justify = "center",
+    font = ("Consolas",14,"bold"),
+    padx = 26,
+    pady = 24,
+    command = lambda:add_on_scr("(", scr_data),
+    )
+    #Button for ( 
+    button_for_right_brac = tk.Button(main_win,
+    text = ")",
+    bg = "#233b44",
+    fg = "#c9c5c2",
+    relief = tk.FLAT,
+    bd = 0,
+    justify = "center",
+    font = ("Consolas",14,"bold"),
+    padx = 26,
+    pady = 24,
+    command = lambda:add_on_scr(")", scr_data),
+    )
+    #button for CE
+    button_for_CE = tk.Button(main_win,
+    text = "CE",
+    bg = "#233b44",
+    fg = "#c9c5c2",
+    relief = tk.FLAT,
+    bd = 0,
+    justify = "center",
+    font = ("Consolas",14,"bold"),
+    padx = 21,
+    pady = 24,
+    command = lambda:CE_ope(scr_data),
     )
     #placing all the widgets
     [
@@ -449,6 +510,9 @@ def main():
         button_for_rem.place(x = 150, y = 102),
         button_for_sqr.place(x = 300, y = 102),
         button_for_sqrt.place(x = 300,y = 166),
+        button_for_left_brac.place(x = 300, y = 250),
+        button_for_right_brac.place(x = 300, y = 334),
+        button_for_CE.place(x = 300, y = 418),
         display_entry.place(x = 0,y = 0,height = 100)
     ]
     main_win.mainloop()
